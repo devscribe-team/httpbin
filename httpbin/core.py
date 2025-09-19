60266c68eef13335910345a4715d189c751e196d
Sam Solano <70192267+samsolano@users.noreply.github.com>
2025-09-19 12:32:45 -0700

    revert

--- httpbin/core.py
+++ httpbin/core.py
@@ -237,15 +237,6 @@ def set_cors_headers(response):
 # ------
 
 
-@app.route("/d")
-def d():
-  var1 = 7
-  var2 = 8
-  c_result = c(var1,var2)
-  b_result = b(var1, var2)
-  print(b_result, c_result)
-  return jsonify({"c_result": c_result, "b_result": b_result})
-
 @app.route("/a")
 def a():
   var1 = 7
